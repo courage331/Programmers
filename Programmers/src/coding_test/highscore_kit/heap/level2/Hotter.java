ba7ef609d91c2fb6a7844986002c937a1bb5a011
courage331 <courage331@gmail.com>
2020-09-07 12:34:49 +0900

    heap, priorityqueue

--- Programmers/src/coding_test/highscore_kit/heap/level2/Hotter.java
+++ Programmers/src/coding_test/highscore_kit/heap/level2/Hotter.java
@@ -1,5 +1,38 @@
 package coding_test.highscore_kit.heap.level2;
 
+import java.util.*;
+
+/*
+ *  
+ * 테스트명 : 베스트 앨범
+ *
+ * @김지민
+ * 
+ * 
+ */
 public class Hotter {
 
+	class Solution {
+		public int solution(int[] scoville, int K) {
+			int answer = 0;
+			PriorityQueue<Integer> pq = new PriorityQueue<>();
+			for (int i = 0; i < scoville.length; i++) {
+				pq.offer(scoville[i]);
+			}
+
+			while (pq.peek() < K) {
+				int a = pq.poll();
+				int b = pq.poll();
+				pq.offer(a + (b * 2));
+				answer++;
+
+				if (pq.size() == 1 && pq.peek() < K) {
+					answer = -1;
+					break;
+				}
+			}
+
+			return answer;
+		}
+	}
 }
--- Programmers/src/coding_test/highscore_kit/heap/level3/Double_Priority_Queue.java
+++ Programmers/src/coding_test/highscore_kit/heap/level3/Double_Priority_Queue.java
@@ -1,5 +1,82 @@
 package coding_test.highscore_kit.heap.level3;
 
-public class Double_Priority_Queue {
+import java.util.*;
 
+
+/*
+ * 
+ * 테스트명 : 프린터
+ * 
+ * @ 김지민
+ * 
+ */
+
+
+class Num implements Comparable<Num>{ 
+
+    int number;
+    
+    public Num (String num){
+        this.number = Integer.parseInt(num);
+    }
+    
+    public int compareTo(Num o){
+
+        if(this.number>o.number){
+            return 1;
+        }else if(this.number < o.number){
+            return  -1;
+        }else 
+            return 0;
+    }
+    
+    public String toString(){
+        String res = Integer.toString(number);
+        return res;
+    }
+    
+}
+
+
+class Double_Priority_Queue {
+    public int[] solution(String[] operations) {
+         PriorityQueue<Num> pq = new PriorityQueue<Num>(); //오름차순으로 정렬한다.   
+         Stack<Num>st = new Stack<Num>(); //오름차순으로 정렬한것이 stack에 담기므로 stack의 peek는 최댓값이다.
+
+        for(int i = 0; i <operations.length; i ++){
+            if(operations[i].split(" ")[0].equals("I")){
+                pq.offer(new Num(operations[i].split(" ")[1]));
+            }
+            else if(operations[i].split(" ")[0].equals("D"))
+            {
+                if(operations[i].split(" ")[1].equals("-1")){
+                    pq.poll();
+                }else{
+                    while(!pq.isEmpty()){
+                        st.push(pq.poll());
+                    }
+                    if(!st.isEmpty()){
+                        st.pop();
+                    }
+                    while(!st.isEmpty()){
+                        pq.offer(st.pop());
+                    }
+
+                }
+ 
+            }  
+        }
+        //answer =[최댓값, 최솟값]
+        int[] answer = new int[2];
+        if(pq.isEmpty()){
+           
+        }else{
+            answer[1]=pq.peek().number;
+            while(!pq.isEmpty()){
+                st.push(pq.poll());
+            }
+            answer[0]=st.peek().number;
+        }
+     return answer;
+    }
 }
