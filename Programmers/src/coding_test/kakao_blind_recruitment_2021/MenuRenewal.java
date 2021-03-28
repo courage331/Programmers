c81023e3fac5702475ae435de53afa6e7355eac3
steve <jmkim1@lunasoft.co.kr>
2021-03-29 08:14:59 +0900

    2021-03-29

--- Programmers/src/coding_test/kakao_blind_recruitment_2021/MenuRenewal.java
+++ Programmers/src/coding_test/kakao_blind_recruitment_2021/MenuRenewal.java
@@ -12,79 +12,119 @@ import java.util.*;
 
 public class MenuRenewal {
 
-    static List<String> arr = new ArrayList();
+    static Map<String, ArrayList<Integer>> infoMap = new HashMap();
 
-    public String[] solution(String[] orders, int[] course) {
+    public int[] solution(String[] info, String[] query) {
+        int[] answer = new int[query.length];
 
-        Map<String, Integer> cookMap = new HashMap<>();
-        for (int i = 0; i < course.length; i++) {
-            for (int j = 0; j < orders.length; j++) {
-                String[] order = orders[j].split("");
-                Arrays.sort(order);
-                int[] chk = new int[order.length];
-                makeCourse(order, chk, course[i], "", 0);
+
+        for (int i = 0; i < info.length; i++) {
+            makeKey(info[i]);
+        }
+
+        ArrayList<String> savekey = new ArrayList();
+        for (Map.Entry<String, ArrayList<Integer>> entry : infoMap.entrySet()) {
+            if (savekey.contains(entry.getKey())) {
+                continue;
+            } else {
+                savekey.add(entry.getKey());
+                Collections.sort(entry.getValue());
             }
-        }
-        for (int k = 0; k < arr.size(); k++) {
-            String temp = arr.get(k);
-            int count = cookMap.containsKey(temp) ? cookMap.get(temp) + 1 : 1;
-            cookMap.put(temp, count);
-        }
-        // System.out.println(arr.toString());
-        // System.out.println(cookMap.toString());
-        List<String> result = new ArrayList<>();
 
-        for (int i = 0; i < course.length; i++) {
-            int max = -1;
-            List<String> temp = new ArrayList<>();
-            for (Map.Entry<String, Integer> entry : cookMap.entrySet()) {
-                //System.out.println("key : " + entry.getKey() + "length"+entry.getKey().length() +", value : " + entry.getValue());
-                if (entry.getKey().length() == course[i] && entry.getValue() >= 2) {
-                    if (max < entry.getValue()) {
-                        max = entry.getValue();
-                        temp.clear();
-                        temp.add(entry.getKey());
-                    } else if (max == entry.getValue()) {
-                        temp.add(entry.getKey());
-                    } else {
+        }
+
+        for (int i = 0; i < query.length; i++) {
+            String[] temp = query[i].split(" ");
+
+            String key = temp[0] + temp[2] + temp[4] + temp[6];
+
+            if (!infoMap.containsKey(key)) {
+                answer[i] = 0;
+                continue;
+            }
+            ArrayList<Integer> arr = infoMap.get(key);
+
+            if ("-".equals(temp[7])) {
+                answer[i] = arr.size();
+            } else {
+                int score = Integer.parseInt(temp[7]);
+                //이분탐색
+                answer[i] = binarySearch(arr, score);
+            }
+
+        }
+
+        //System.out.println(infoMap.toString());
+        return answer;
+
+    }
+
+    public void makeKey(String info) {
+
+        String[] temp = info.split(" ");
+
+        String[] language = {temp[0], "-"};
+        String[] part = {temp[1], "-"};
+        String[] career = {temp[2], "-"};
+        String[] food = {temp[3], "-"};
+        int score = Integer.parseInt(temp[4]);
+
+
+        for (int i = 0; i < language.length; i++) {
+            for (int j = 0; j < part.length; j++) {
+                for (int k = 0; k < career.length; k++) {
+                    for (int p = 0; p < food.length; p++) {
+                        String key = language[i] + part[j] + career[k] + food[p];
+                        if (infoMap.containsKey(key)) {
+                            ArrayList<Integer> arr = infoMap.get(key);
+                            arr.add(score);
+                            //Collections.sort(arr);
+                            infoMap.put(key, arr);
+                        } else {
+                            ArrayList<Integer> arr = new ArrayList();
+                            arr.add(score);
+                            infoMap.put(key, arr);
+                        }
 
                     }
                 }
             }
-            for (int k = 0; k < temp.size(); k++) {
-                result.add(temp.get(k));
-            }
-
         }
-        Collections.sort(result);
 
-        String[] answer = result.toArray(new String[result.size()]);
-        return answer;
+        return;
     }
 
-    public void makeCourse(String[] order, int[] chk, int course, String word, int idx) {
+    public int binarySearch(ArrayList<Integer> arr, int score) {
 
-        if (course > order.length) {
-            return;
+        int mid = 0;
+        int end = arr.size();
+        int start = 0;
+        if (arr.size() == 1) {
+            int a = arr.get(0) >= score ? 1 : 0;
+            return a;
         }
 
-        if (word.length() == course) {
-            arr.add(word);
-            return;
+        if (arr.get(arr.size() - 1) < score) {
+            return 0;
         }
 
 
-        for (int i = idx; i < order.length; i++) {
-            if (chk[idx] != 0) {
-                continue;
-            }
-            word += order[i];
-            chk[idx] = 1;
-            makeCourse(order, chk, course, word, i + 1);
-            chk[idx] = 0;
-            word = word.substring(0, word.length() - 1);
+        if (arr.get(0) >= score) {
+            return arr.size();
         }
+
+        while (end > start) // end가 start보다 같거나 작아지면, 그 값이 Lower Bound이므로 반복을 종료한다.
+        {
+            mid = (start + end) / 2;
+            //System.out.println("arr : "+arr.toString() + " score : "+score+" start : "+start+" mid : "+mid+" end : "+end);
+            if (arr.get(mid) >= score) // 중간값이 원하는 값보다 크거나 같을 경우, 끝값을 중간값으로 설정하여 다시 탐색한다.
+                end = mid;
+            else start = mid + 1; // 중간값이 원하는 값보다 작을 경우, 시작값을 중간값+1로 설정하여 다시 탐색한다.
+            // System.out.println("arr : "+arr.toString() + " score : "+score+" start : "+start+" mid : "+mid+" end : "+end);
+        }
+        return arr.size() - start;
 
     }
 }
+
 
